46aa5b24a345b234094e9ac9a422e3720e30b079
Jordan Hunt <jordan.hunt@g.austincc.edu>
2026-04-25 21:03:26 -0500

    Removed <string.h> and moved it to common.h

--- include/map.h
+++ include/map.h
@@ -6,7 +6,6 @@
 #include "maptile.h"
 #include <SDL2/SDL_rect.h>
 #include <stdio.h>
-#include <string.h>
 void LoadMap(LevelData *levelData, const char *filePath);
 void DrawMap(LevelData *levelData, SDL_Renderer *renderer);
 enum MapTile GetMapTile(LevelData *levelData, int x, int y);
