a68056f9e71794e7da9f18dcec42167b7683c466
Jordan Hunt <jordan.hunt@g.austincc.edu>
2026-05-13 22:45:47 -0500

    collisionSystem.c complete

--- src/systems/collisionSystem.c
+++ src/systems/collisionSystem.c
@@ -1,1 +1,17 @@
 #include "../../include/systems/collisionSystem.h"
+
+void CollisionSystem(GameContext *gameContext, SDL_Renderer *renderer) {
+  if (ECS_HasComponent(gameContext->playerEntity, COMPONENT_POSITION) ==
+      false) {
+    return;
+  }
+  Velocity *velocity = ECS_GetVelocity(gameContext->playerEntity);
+  if (velocity->deltaRow == 0 && velocity->deltaColumn == 0) {
+    return;
+  }
+  Position *position = ECS_GetPosition(gameContext->playerEntity);
+  MapTile collisionTile =
+      GetMapTile(&gameContext->levelData, position->row, position->column);
+  HandlePlayerTileCollision(gameContext, position->row, position->column,
+                            collisionTile);
+}
