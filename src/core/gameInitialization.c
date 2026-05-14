4cca8a9bf91580e14945445ca67e3c6690c8cd50
Jordan "Heroic" Hunt <jordan.hunt@g.austincc.edu>
2026-05-14 13:53:22 -0500

    I'll be able to test my code once I get home but I think my gameInit should work. I think I made too many changes to my project and I should probably change a little bit test it, and then move on rather than these sorts of big endevors.

--- src/core/gameInitialization.c
+++ src/core/gameInitialization.c
@@ -1,8 +1,50 @@
 #include "../../include/gameinitialization.h"
 
-bool InitializeSDL(SDLContext *sdlContext) {
+bool InitializeSDL(SDLContext* sdlContext) {
   if (SDL_Init(SDL_INIT_VIDEO < 0)) {
     printf("[gameInitialization.c] - SDL failed to initialize: %s\n",
            SDL_GetError());
+    return false;
   }
+  sdlContext->window = SDL_CreateWindow("Hac-Man", SDL_WINDOWPOS_UNDEFINED,
+                                        SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                                        SCREEN_HEIGHT, SDL_WINDOW_HIDDEN);
+  sdlContext->renderer =
+      SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  return true;
+}
+
+bool InitializePlayer(GameContext* gameContext, Entity* player) {
+    ECS_AddComponent(player, COMPONENT_POSITION | COMPONENT_VELOCITY | COMPONENT_RENDERABLE | COMPONENT_PLAYER_CONTROLLED);
+    bool isSpawnLocationFound =false;
+    for(int row = 0 ; row < MAP_ROWS; row++){
+        for(int column = 0; column < MAP_COLUMNS;   column++){
+            if(GetMapTile(&gameContext->levelData,  row,  column) == TILE_PLAYER )){
+                Position *playerPosition = ECS_GetPosition(player);
+                playerPosition->row=row;
+                playerPosition->column=column;    SetMapTile(&gameContext->levelData, row, column, TILE_EMPTY); isSpawnLocationFound = true;   }
+        }
+    }
+    if (isSpawnLocationFound == false) {
+        printf("[gameInitialization.c - NO TILE_PLAYER spawn location found! \n");
+        return false;
+    }
+    Velocity *velocity = ECS_GetVelocity(player); velocity->deltaRow=0; velocity->deltaColumn=0;
+    Renderable *renderData = ECS_GetRenderable(player); renderData->red = 255; renderData->green=255; renderData->blue = 0; renderData->alpha = 255;
+    renderData->width=MAP_GRID_CELL_SIZE; renderData->height=MAP_GRID_CELL_SIZE;
+    return true;
+}
+
+void InitializeSystems(){
+    ECS_RegisterSystem(InputSystem          );
+    ECS_RegisterSystem(MovementSystem  );
+    ECS_RegisterSystem(CollisionSystem );
+    ECS_RegisterSystem(RenderSystem );
+}
+
+
+void Shutdown(SDLContext *sdlContext){
+    SDL_DestroyRenderer(sdlContext->renderer);
+    SDL_DestroyWindow(sdlContext->gameWindow);
+    SDL_Quit();
 }
