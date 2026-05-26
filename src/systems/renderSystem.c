0175f453a1f77207d52fe69286034d0c242b3f48
Jordan "Heroic" Hunt <jordan.hunt@g.austincc.edu>
2026-05-26 12:46:40 -0500

    Updated renderSystem.c to draw pixel positition because I updated the movementSystem. Small Git pushes probably for the best.

--- src/systems/renderSystem.c
+++ src/systems/renderSystem.c
@@ -1,16 +1,23 @@
 #include "../../include/systems/renderSystem.h"
-void RenderSystem(GameContext *gameContext, SDL_Renderer *renderer) {
+void RenderSystem(GameContext *gameContext, SDL_Renderer *renderer)
+{
   int activeEntitiesCount = ECS_GetActiveEntitiesCount();
-  for (int i = 0; i < activeEntitiesCount; i++) {
+  for (int i = 0; i < activeEntitiesCount; i++)
+  {
     Entity activeEntity = ECS_GetActiveEntity(i);
     if (ECS_HasComponents(activeEntity,
-                          COMPONENT_POSITION | COMPONENT_RENDERABLE) == false) {
+                          COMPONENT_POSITION | COMPONENT_RENDERABLE) == false)
+    {
       continue;
     }
     Position *position = ECS_GetPosition(activeEntity);
     Renderable *renderData = ECS_GetRenderable(activeEntity);
-    SDL_Rect renderRect = {position->column * MAP_GRID_CELL_SIZE,
-                           position->row * MAP_GRID_CELL_SIZE,
+
+    // Convert position to pixels rather than tiles
+    float pixelX = (position->column + position->offsetX) * MAP_GRID_CELL_SIZE;
+    float pixelY = (position->row + position->offsetY) * MAP_GRID_CELL_SIZE;
+    SDL_Rect renderRect = {(int)pixelX,
+                           (int)pixelY,
                            renderData->width, renderData->height};
     SDL_SetRenderDrawColor(renderer, renderData->red, renderData->green,
                            renderData->blue, renderData->alpha);
