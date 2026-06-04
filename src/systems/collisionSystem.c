b707282e1fd05f4e9a2f3942f92b5ed6fe7a1de7
Jordan "Heroic" Hunt <jordan.hunt@g.austincc.edu>
2026-06-04 11:55:59 -0500

    whitespace

--- src/systems/collisionSystem.c
+++ src/systems/collisionSystem.c
@@ -1,14 +1,18 @@
 #include "../../include/systems/collisionSystem.h"
 #include <stdio.h>
-static Entity FindEdibleAt(int row, int column) {
+static Entity FindEdibleAt(int row, int column)
+{
   int activeCount = ECS_GetActiveEntitiesCount();
   Entity edibleEntity;
-  for (int i = 0; i < activeCount; i++) {
+  for (int i = 0; i < activeCount; i++)
+  {
     edibleEntity = ECS_GetActiveEntity(i);
     if (ECS_HasComponents(edibleEntity,
-                          COMPONENT_POSITION | COMPONENT_EDIBLE) == true) {
+                          COMPONENT_POSITION | COMPONENT_EDIBLE) == true)
+    {
       Position *ediblePosition = ECS_GetPosition(edibleEntity);
-      if (ediblePosition->row == row && ediblePosition->column == column) {
+      if (ediblePosition->row == row && ediblePosition->column == column)
+      {
         return edibleEntity;
       }
     }
@@ -16,70 +20,83 @@ static Entity FindEdibleAt(int row, int column) {
   return ENTITY_NULL;
 }
 // *Consume* is a PAC-MAN pun. I would usually use "handle"
-static void ConsumeEdibleEntity(GameContext *gameContext, Entity edibleEntity) {
+static void ConsumeEdibleEntity(GameContext *gameContext, Entity edibleEntity)
+{
   Edible *justEatenEdible = ECS_GetEdible(edibleEntity);
   gameContext->currentScore += justEatenEdible->scoreValue;
   if (justEatenEdible->typeEaten == POWER_PELLET ||
-      justEatenEdible->typeEaten == DOT) {
+      justEatenEdible->typeEaten == DOT)
+  {
     ReduceRemainingPellets(gameContext);
   }
-  if (justEatenEdible->typeEaten == POWER_PELLET) {
+  if (justEatenEdible->typeEaten == POWER_PELLET)
+  {
     TriggerFrightenedMode(gameContext);
   }
-  if(justEatenEdible->typeEaten == FRIGHTENED_GHOST){
-      Ghost *ghost = ECS_GetGhost(edibleEntity);
-      ghost->ghostMode = GHOSTMODE_EATEN_EYES;
-      printf("[collision.c] - A frightened ghost has been eaten! Current "
-             "Score: %d\n",
-             gameContext->currentScore);
+  if (justEatenEdible->typeEaten == FRIGHTENED_GHOST)
+  {
+    Ghost *ghost = ECS_GetGhost(edibleEntity);
+    ghost->ghostMode = GHOSTMODE_EATEN_EYES;
+    printf("[collision.c] - A frightened ghost has been eaten! Current "
+           "Score: %d\n",
+           gameContext->currentScore);
   }
   ECS_DestroyEntity(edibleEntity);
-  if (gameContext->isRoundWon == false) {
+  if (gameContext->isRoundWon == false)
+  {
     CheckForRoundWon(gameContext);
   }
 }
 
-void CollisionSystem(GameContext *gameContext, SDL_Renderer *renderer) {
+void CollisionSystem(GameContext *gameContext, SDL_Renderer *renderer)
+{
   (void)renderer;
   Entity playerEntity = gameContext->playerEntity;
-  if (ECS_HasComponent(playerEntity, COMPONENT_POSITION) == false) {
+  if (ECS_HasComponent(playerEntity, COMPONENT_POSITION) == false)
+  {
     return;
   }
-  if (ECS_HasComponent(playerEntity, COMPONENT_VELOCITY) == false) {
+  if (ECS_HasComponent(playerEntity, COMPONENT_VELOCITY) == false)
+  {
     printf("[collisionSystem.c] - Object does not have velocity component");
     return;
   }
   Position *playerPosition = ECS_GetPosition(gameContext->playerEntity);
   Entity edible = FindEdibleAt(playerPosition->row, playerPosition->column);
-  if (edible != ENTITY_NULL) {
+  if (edible != ENTITY_NULL)
+  {
     ConsumeEdibleEntity(gameContext, edible);
   }
   int activeEntitesCount = ECS_GetActiveEntitiesCount();
-  for (int i = 0; i < activeEntitesCount; i++) {
+  for (int i = 0; i < activeEntitesCount; i++)
+  {
     Entity activeEntity = ECS_GetActiveEntity(i);
     // Skip over non-ghosts
-    if (ECS_HasComponent(activeEntity, COMPONENT_GHOST) == false) {
+    if (ECS_HasComponent(activeEntity, COMPONENT_GHOST) == false)
+    {
       continue;
     }
     // Skip over the player
-    if (activeEntity == playerEntity) {
+    if (activeEntity == playerEntity)
+    {
       continue;
     }
 
-Ghost *ghost = ECS_GetGhost(activeEntity);
+    Ghost *ghost = ECS_GetGhost(activeEntity);
     Position *ghostPosition = ECS_GetPosition(activeEntity);
 
     if (ghostPosition->row != playerPosition->row ||
-        ghostPosition->column != playerPosition->column) {
+        ghostPosition->column != playerPosition->column)
+    {
       continue;
     }
     // Kill the player if the player collides with a NOT frightened and NOT EYE_MODE ghost
-  if(ghost->ghostMode != GHOST_MODE_FRIGHTENED &&
-          ghost->ghostMode != GHOST_MODE_EATEN_EYES)
+    if (ghost->ghostMode != GHOST_MODE_FRIGHTENED &&
+        ghost->ghostMode != GHOST_MODE_EATEN_EYES)
       // Player dies
       TriggerPlayerDeath(gameContext);
-      printf("[collision.c] - PAC-MAN has collided with a Ghost!\n");
-      break;
-    }
+    printf("[collision.c] - PAC-MAN has collided with a Ghost!\n");
+    break;
   }
+}
 }
--- src/systems/ghostSystem.c
+++ src/systems/ghostSystem.c
@@ -1,67 +1,92 @@
 #include "../../include/systems/ghostSystem.h"
-static int DistanceSquared(int row1, int column1, int row2, int column2) {
+static int DistanceSquared(int row1, int column1, int row2, int column2)
+{
   int distanceSquared = 0;
   int deltaRow = row2 - row1;
   int deltaColumn = column2 - column1;
   distanceSquared = (deltaRow * deltaRow) + (deltaColumn * deltaColumn);
   return distanceSquared;
 }
-static int DirectionToDeltaRow(Direction direction) {
-  if (direction == UP) {
+static int DirectionToDeltaRow(Direction direction)
+{
+  if (direction == UP)
+  {
     return -1;
-  } else if (direction == DOWN) {
+  }
+  else if (direction == DOWN)
+  {
     return 1;
-  } else {
+  }
+  else
+  {
     return 0;
   }
 }
-static int DirectionToDeltaColumn(Direction direction) {
-  if (direction == LEFT) {
+static int DirectionToDeltaColumn(Direction direction)
+{
+  if (direction == LEFT)
+  {
     return -1;
-  } else if (direction == RIGHT) {
+  }
+  else if (direction == RIGHT)
+  {
     return 1;
-  } else {
+  }
+  else
+  {
     return 0;
   }
 }
-static Direction OppositeDirection(Direction direction) {
-  switch (direction) {
-  case UP: {
+static Direction OppositeDirection(Direction direction)
+{
+  switch (direction)
+  {
+  case UP:
+  {
     return DOWN;
   }
-  case DOWN: {
+  case DOWN:
+  {
     return UP;
   }
-  case LEFT: {
+  case LEFT:
+  {
     return RIGHT;
   }
-  case RIGHT: {
+  case RIGHT:
+  {
     return LEFT;
   }
-  default: {
+  default:
+  {
     return ZERO_DIRECTION;
   }
   }
 }
-static void MoveGhostRandomly(Entity ghostEntity, LevelData *levelData) {
+static void MoveGhostRandomly(Entity ghostEntity, LevelData *levelData)
+{
   Position *ghostPosition = ECS_GetPosition(ghostEntity);
   Velocity *ghostVelocity = ECS_GetVelocity(ghostEntity);
   Ghost *ghost = ECS_GetGhost(ghostEntity);
 
-  if (ghost->ghostType != 6) {
+  if (ghost->ghostType != 6)
+  {
     return;
   }
   // Only change direction when centered on a tile to fix the jitter. It was
   // changing direction every frame
-  if (IsGhostCentered(ghostPosition) == false) {
+  if (IsGhostCentered(ghostPosition) == false)
+  {
     return;
   }
   Direction directions[4] = {UP, DOWN, LEFT, RIGHT};
   Direction validDirections[4];
   int validDirectionCount = 0;
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < 4; i++)
+  {
     Direction possibleDirection = directions[i];
-    if (possibleDirection == OppositeDirection(ghost->currentDirection)) {
+    if (possibleDirection == OppositeDirection(ghost->currentDirection))
+    {
       continue;
     }
     int newRow = ghostPosition->row + DirectionToDeltaRow(possibleDirection);
@@ -69,17 +94,20 @@ static void MoveGhostRandomly(Entity ghostEntity, LevelData *levelData) {
         ghostPosition->column + DirectionToDeltaColumn(possibleDirection);
     // Bounds Check for edge cases, don't go off the map
     if (newRow < 0 || newRow >= MAP_ROWS || newColumn < 0 ||
-        newColumn >= MAP_COLUMNS) {
+        newColumn >= MAP_COLUMNS)
+    {
       continue;
     }
     MapTile mapTile = GetMapTile(levelData, newRow, newColumn);
-    if (mapTile == TILE_WALL || mapTile == TILE_GHOST_DOOR) {
+    if (mapTile == TILE_WALL || mapTile == TILE_GHOST_DOOR)
+    {
       continue;
     }
     validDirections[validDirectionCount] = possibleDirection;
     validDirectionCount += 1;
   }
-  if (validDirectionCount > 0) {
+  if (validDirectionCount > 0)
+  {
     Direction chosenDirection = validDirections[rand() % validDirectionCount];
     ghost->currentDirection = chosenDirection;
     ghostVelocity->deltaRow = DirectionToDeltaRow(chosenDirection);
@@ -90,23 +118,27 @@ static void MoveGhostRandomly(Entity ghostEntity, LevelData *levelData) {
 }
 
 static void MoveGhostTowardTarget(Entity ghostEntity, int targetRow,
-                                  int targetColumn, LevelData *levelData) {
+                                  int targetColumn, LevelData *levelData)
+{
   Position *ghostPosition = ECS_GetPosition(ghostEntity);
   Velocity *ghostVelocity = ECS_GetVelocity(ghostEntity);
   Ghost *ghost = ECS_GetGhost(ghostEntity);
 
   // Only change direction when centered on a tile to fix the jitter. It was
   // changing direction every frame
-  if (IsGhostCentered(ghostPosition) == false) {
+  if (IsGhostCentered(ghostPosition) == false)
+  {
     return;
   }
   Direction directions[4] = {UP, DOWN, LEFT, RIGHT};
   Direction directionToTarget = ZERO_DIRECTION;
   int bestDistanceToTarget = INT_MAX;
-  for (size_t i = 0; i < (sizeof(directions) / sizeof(directions[0])); i++) {
+  for (size_t i = 0; i < (sizeof(directions) / sizeof(directions[0])); i++)
+  {
     Direction candidateDirection = directions[i];
     // No backwards movement is allowed for ghosts
-    if (candidateDirection == OppositeDirection(ghost->currentDirection)) {
+    if (candidateDirection == OppositeDirection(ghost->currentDirection))
+    {
       continue;
     }
     int newRow = ghostPosition->row + DirectionToDeltaRow(candidateDirection);
@@ -114,25 +146,30 @@ static void MoveGhostTowardTarget(Entity ghostEntity, int targetRow,
         ghostPosition->column + DirectionToDeltaColumn(candidateDirection);
     // Bounds Check for edge cases, don't go off the map
     if (newRow < 0 || newRow >= MAP_ROWS || newColumn < 0 ||
-        newColumn >= MAP_COLUMNS) {
+        newColumn >= MAP_COLUMNS)
+    {
       continue;
     }
     // Can't move through walls
     MapTile tileAheadOfGhost = GetMapTile(levelData, newRow, newColumn);
-    if (tileAheadOfGhost == TILE_WALL) {
+    if (tileAheadOfGhost == TILE_WALL)
+    {
       continue;
     }
-    if (tileAheadOfGhost == TILE_GHOST_DOOR) {
+    if (tileAheadOfGhost == TILE_GHOST_DOOR)
+    {
       continue;
     }
     int distanceSquared =
         DistanceSquared(targetRow, targetColumn, newRow, newColumn);
-    if (distanceSquared < bestDistanceToTarget) {
+    if (distanceSquared < bestDistanceToTarget)
+    {
       bestDistanceToTarget = distanceSquared;
       directionToTarget = candidateDirection;
     }
   }
-  if (directionToTarget != ZERO_DIRECTION) {
+  if (directionToTarget != ZERO_DIRECTION)
+  {
     ghost->currentDirection = directionToTarget;
     ghostVelocity->deltaRow = DirectionToDeltaRow(directionToTarget);
     ghostVelocity->deltaColumn = DirectionToDeltaColumn(directionToTarget);
@@ -143,7 +180,8 @@ static void MoveGhostTowardTarget(Entity ghostEntity, int targetRow,
 }
 // After being eaten, turn into eyes and return from current position back into
 // the ghost house
-static void UpdateGhostEyes(Entity ghostEntity, GameContext *gameContext) {
+static void UpdateGhostEyes(Entity ghostEntity, GameContext *gameContext)
+{
   Position *ghostPosition = ECS_GetPosition(ghostEntity);
   Ghost *ghost = ECS_GetGhost(ghostEntity);
   Velocity *ghostVelocity = ECS_GetVelocity(ghostEntity);
@@ -152,7 +190,8 @@ static void UpdateGhostEyes(Entity ghostEntity, GameContext *gameContext) {
                         GHOST_HOUSE_ENTRANCE_COLUMN, &gameContext->levelData);
   // Enter the ghost house and reset the ghost to scatter to get back at it.
   if (ghostPosition->row == GHOST_HOUSE_ENTRANCE_ROW &&
-      ghostPosition->column == GHOST_HOUSE_ENTRANCE_COLUMN) {
+      ghostPosition->column == GHOST_HOUSE_ENTRANCE_COLUMN)
+  {
     ghost->ghostMode = GHOSTMODE_SCATTER;
     ghostVelocity->tilesPerSecond = GHOST_SPEED;
     ghostPosition->offsetX = 0.0f;
@@ -160,13 +199,15 @@ static void UpdateGhostEyes(Entity ghostEntity, GameContext *gameContext) {
     printf("[ghostSystem.c] - Ghost has respawned.\n");
   }
 }
-static void UpdateGhostHouseExit(Entity ghostEntity, GameContext *gameContext) {
+static void UpdateGhostHouseExit(Entity ghostEntity, GameContext *gameContext)
+{
   Position *ghostPosition = ECS_GetPosition(ghostEntity);
   Ghost *ghost = ECS_GetGhost(ghostEntity);
   Velocity *ghostVelocity = ECS_GetVelocity(ghostEntity);
   int dotsEaten = NUMBER_OF_DOTS - gameContext->remainingPellets;
   // Ghosts exit the Ghost House after a threshold is met
-  if (dotsEaten < ghost->exitGhostHouseThreshold) {
+  if (dotsEaten < ghost->exitGhostHouseThreshold)
+  {
     ghostVelocity->deltaRow = 0;
     ghostVelocity->deltaColumn = 0;
     return;
@@ -175,7 +216,8 @@ static void UpdateGhostHouseExit(Entity ghostEntity, GameContext *gameContext) {
   MoveGhostTowardTarget(ghostEntity, GHOST_HOUSE_ENTRANCE_ROW,
                         GHOST_HOUSE_ENTRANCE_COLUMN, &gameContext->levelData);
   if (ghostPosition->row == GHOST_HOUSE_ENTRANCE_ROW &&
-      ghostPosition->column == GHOST_HOUSE_ENTRANCE_COLUMN) {
+      ghostPosition->column == GHOST_HOUSE_ENTRANCE_COLUMN)
+  {
     ghost->ghostMode = GHOSTMODE_SCATTER;
     printf("[ghostSystem.c] - Ghost exited house.\n");
   }
@@ -184,15 +226,18 @@ static void UpdateGhostHouseExit(Entity ghostEntity, GameContext *gameContext) {
 // describes each ghosts behavior Right now I'm just focusing on blinky cause
 // he's the easier. This ghost targets the player directly
 static void GetChaseTarget(Entity entity, GameContext *gameContext,
-                           int *targetRow, int *targetColumn) {
+                           int *targetRow, int *targetColumn)
+{
   Ghost *ghost = ECS_GetGhost(entity);
   Position *playerPosition = ECS_GetPosition(gameContext->playerEntity);
   PlayerControlled *playerControlled =
       ECS_GetPlayerControlled(gameContext->playerEntity);
-  switch (ghost->ghostType) {
+  switch (ghost->ghostType)
+  {
   // Blinky directly targets the player's current location
   // "oikake" (追い掛け) is to purse
-  case GHOSTTYPE_BLINKY: {
+  case GHOSTTYPE_BLINKY:
+  {
     *targetRow = playerPosition->row;
     *targetColumn = playerPosition->column;
     break;
@@ -200,7 +245,8 @@ static void GetChaseTarget(Entity entity, GameContext *gameContext,
   // Pink attempts to get "ahead" of the player to ambush them by going to
   // wherever 4 tiles ahead are.
   // "machibuse" (待ち伏せ) is to ambush
-  case GHOSTTYPE_PINKY: {
+  case GHOSTTYPE_PINKY:
+  {
     *targetRow = playerPosition->row +
                  (DirectionToDeltaRow(playerControlled->currentDirection) * 4);
     *targetColumn =
@@ -211,16 +257,19 @@ static void GetChaseTarget(Entity entity, GameContext *gameContext,
     // TODO: Add logic for Inky and Clyde, will default to BLINKY for now.
     // Inky is the most complex tracking. Least predictable most erratic
     // "kimagure" (気まぐれ) means to be "fickle" or "capricious"
-  case GHOSTTYPE_INKY: {
+  case GHOSTTYPE_INKY:
+  {
     break;
   }
   // TODO: Add logic for Inky and Clyde, will default to BLINKY for now.
   // Clyde uses a proximity tracking
   //  "otoboke" (お惚け) is to feign ignorance
-  case GHOSTTYPE_CLYDE: {
+  case GHOSTTYPE_CLYDE:
+  {
     break;
   }
-  default: {
+  default:
+  {
     // TODO: Add logic for Inky and Clyde, will default to BLINKY for now.
     *targetRow = playerPosition->row;
     *targetColumn = playerPosition->column;
@@ -228,59 +277,75 @@ static void GetChaseTarget(Entity entity, GameContext *gameContext,
   }
   }
 }
-void GhostSystem(GameContext *gameContext, SDL_Renderer *renderer) {
+void GhostSystem(GameContext *gameContext, SDL_Renderer *renderer)
+{
   (void)renderer;
   int activeEntitiesCount = ECS_GetActiveEntitiesCount();
-  for (int i = 0; i < activeEntitiesCount; i++) {
+  for (int i = 0; i < activeEntitiesCount; i++)
+  {
     Entity activeEntity = ECS_GetActiveEntity(i);
     if (ECS_HasComponents(activeEntity, COMPONENT_GHOST | COMPONENT_POSITION |
-                                            COMPONENT_VELOCITY) == false) {
+                                            COMPONENT_VELOCITY) == false)
+    {
       continue;
     }
     Ghost *ghost = ECS_GetGhost(activeEntity);
     Velocity *ghostVelocity = ECS_GetVelocity(activeEntity);
-    switch (ghost->ghostMode) {
+    switch (ghost->ghostMode)
+    {
     case GHOSTMODE_IN_GHOSTHOUSE:
-    case GHOSTMODE_EXIT_GHOSTHOUSE: {
+    case GHOSTMODE_EXIT_GHOSTHOUSE:
+    {
       UpdateGhostHouseExit(activeEntity, gameContext);
       break;
     }
-    case GHOSTMODE_EATEN_EYES: {
+    case GHOSTMODE_EATEN_EYES:
+    {
       UpdateGhostEyes(activeEntity, gameContext);
       break;
     }
-    case GHOSTMODE_FRIGHTENED: {
-      if (gameContext->isFrightenedGhostModeActive == false) {
+    case GHOSTMODE_FRIGHTENED:
+    {
+      if (gameContext->isFrightenedGhostModeActive == false)
+      {
         ghost->ghostMode = GHOSTMODE_SCATTER;
         ghostVelocity->tilesPerSecond = GHOST_SPEED;
         // Remove the edible component
-        if(ECS_HasComponent(activeEntity,COMPONENT_EDIBLE) == true){
-            ECS_RemoveComponent(activeEntity, COMPONENT_EDIBLE);
+        if (ECS_HasComponent(activeEntity, COMPONENT_EDIBLE) == true)
+        {
+          ECS_RemoveComponent(activeEntity, COMPONENT_EDIBLE);
         }
-      } else {
+      }
+      else
+      {
         ghostVelocity->tilesPerSecond = GHOST_SPEED_FRIGHTENED;
         MoveGhostRandomly(activeEntity, &gameContext->levelData);
       }
       break;
     }
     case GHOSTMODE_CHASE:
-    case GHOSTMODE_SCATTER: {
-      if (gameContext->isFrightenedGhostModeActive == true) {
+    case GHOSTMODE_SCATTER:
+    {
+      if (gameContext->isFrightenedGhostModeActive == true)
+      {
         ghost->ghostMode = GHOSTMODE_FRIGHTENED;
 
         // When Frigtened it is now edible
-        ECS_AddComponent(activeEntity,COMPONENT_EDIBLE);
-        Edible *edibleGhost= ECS_GetEdible(activeEntity);
+        ECS_AddComponent(activeEntity, COMPONENT_EDIBLE);
+        Edible *edibleGhost = ECS_GetEdible(activeEntity);
         edibleGhost->typeEaten = FRIGHTENED_GHOST;
         edibleGhost->scoreValue = BASE_GHOST_SCORE;
         MoveGhostRandomly(activeEntity, &gameContext->levelData);
-
-      } else if (gameContext->currentGhostMode == GHOSTMODE_SCATTER) {
+      }
+      else if (gameContext->currentGhostMode == GHOSTMODE_SCATTER)
+      {
         ghost->ghostMode = GHOSTMODE_SCATTER;
         MoveGhostTowardTarget(activeEntity, ghost->scatterTargetRow,
                               ghost->scatterTargetColumn,
                               &gameContext->levelData);
-      } else {
+      }
+      else
+      {
         ghost->ghostMode = GHOSTMODE_CHASE;
         int targetRow = 0;
         int targetColumn = 0;
@@ -290,7 +355,8 @@ void GhostSystem(GameContext *gameContext, SDL_Renderer *renderer) {
       }
       break;
     }
-    default: {
+    default:
+    {
       break;
     }
     }
