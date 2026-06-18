85102782c38aa558760e6e92455ca625c9c3d6c9
Jordan "Heroic" Hunt <jordan.hunt@g.austincc.edu>
2026-06-18 10:15:07 -0500

    Updated audio.h to mirror the stuff I saw online
    I think this is a better way to do it honestly.

--- include/audio.h
+++ include/audio.h
@@ -1,6 +1,27 @@
 #ifndef AUDIO_H
 #define AUDIO_H
 #include "common.h"
-Mix_Chunk LoadSound(char *soundFile);
-void PlaySound(Mix_Chunk *soundEffect);
+typedef enum SoundEffectId {
+  SOUNDEFFECT_WAKA0,
+  SOUNDEFFECT_WAKA1,
+  SOUNDEFFECT_POWER_PELLET,
+  SOUNDEFFECT_GHOST_EATEN,
+  SOUNDEFFECT_PLAYER_DEATH,
+  SOUNDEFFECT_FRUIT,
+  SOUNDEFFECT_COUNT
+} SoundEffectId;
+typedef struct AudioState {
+  Mix_Chunk *soundEffects[SOUNDEFFECT_COUNT];
+  Mix_Chunk *waka0;
+  Mix_Chunk *waka1;
+  int wakaSoundEffectToggle;
+  Mix_Music *backgroundMusic;
+} AudioState;
+bool InitializeAudio(AudioState *audioState);
+bool AudioLoadAssets(AudioState *audioState);
+void AudioPlaySoundEffect(AudioState *audioState, SoundEffectId soundEffectId);
+void AudioPlayWaka(AudioState *audioState);
+void AudioPlayMusic(AudioState *audioState, bool isLooping);
+void AudioResetWaka(AudioState *audioState);
+void ShutdownAudio(AudioState *audioState);
 #endif
